8c8e59e8cc9ba3ea200d0d374e064d6b5924c464
AhmedSami019 <easami019192@gmail.com>
2025-10-07 18:14:35 +0600

    positiv, negative and Zero checking problem solved

---
+++ isPositivOrNot.js
@@ -0,0 +1,9 @@
+const number = 0;
+
+if(number > 0){
+    console.log(number, "is a positive number");
+}else if(number === 0 ){
+    console.log(number, "is Zero");
+}else{
+    console.log(number, "is a negative number");
+}
