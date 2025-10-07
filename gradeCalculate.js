dde660def0be55e9574a274c9d196c55b4fdd9e6
AhmedSami019 <easami019192@gmail.com>
2025-10-07 19:13:40 +0600

    caluculate grade problem solved

---
+++ gradeCalculate.js
@@ -0,0 +1,17 @@
+const marks = 80;
+
+if(marks >= 80){
+    console.log(marks, "A+");
+}else if(marks >= 70){
+    console.log(marks, "A");
+}else if(marks >= 60){
+    console.log(marks, "A-");
+}else if(marks >= 50){
+    console.log(marks, "B");
+}else if(marks >= 40){
+    console.log(marks, "C");
+}else if(marks >= 33){
+    console.log(marks, "D");
+}else{
+    console.log(marks, "F");
+}
