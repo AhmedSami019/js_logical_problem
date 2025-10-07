caa0ea5d76db745d34fd9d9a48b34459700a09d3
AhmedSami019 <easami019192@gmail.com>
2025-10-07 19:03:55 +0600

    divisible nmber by 5 and 11 is solved

---
+++ divisibleBy_5_And_11.js
@@ -0,0 +1,7 @@
+const number = 15
+
+if(number % 5 === 0 && number % 11 === 0 ){
+    console.log(number, "this number is divisible by 5 and 11");
+}else{
+    console.log(number, "this number is not divisible by 5 and 11");
+}
