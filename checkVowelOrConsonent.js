705b627aac86cb3d5bb183a6eb258c0aea3a88f4
AhmedSami019 <easami019192@gmail.com>
2025-10-07 19:28:26 +0600

    is vowel or consonant plroblem solved

---
+++ checkVowelOrConsonent.js
@@ -0,0 +1,11 @@
+const letter = "E"
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+if(vowels.includes(letter.toLowerCase())){
+    console.log(letter , "is a vowel");
+}else{
+    console.log(letter, "is a consonant");
+}
+
+
+
