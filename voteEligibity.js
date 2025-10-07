95f4f42b044876e8d277d1f4997e03583d0726d5
AhmedSami019 <easami019192@gmail.com>
2025-10-07 18:58:43 +0600

    vote eligibility problem solved

--- voteEligibity.js
+++ voteEligibity.js
@@ -1,6 +1,13 @@
-const age = 34;
-if(age >= 18){
-    console.log(age, "age. this person is Eligible for vote");
-}else{
-    console.log(age, "age, this person is not Eligible for vote");
-}
+const age = 4;
+
+age >= 18 ? 
+console.log(age, "age. this person is Eligible for vote"):
+console.log(age, "age, this person is not Eligible for vote");
+
+
+
+// if(age >= 18){
+//     console.log(age, "age. this person is Eligible for vote");
+// }else{
+//     console.log(age, "age, this person is not Eligible for vote");
+// }
