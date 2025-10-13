8c4b1d06600d791a3d05e4574f66a43981da8bc2
AhmedSami019 <easami019192@gmail.com>
2025-10-13 19:05:27 +0600

    simple calculator is compleate:

---
+++ simpleCalculator.js
@@ -0,0 +1,14 @@
+const num1 = 54;
+const num2 = 32;
+
+const operator = '*';
+
+if(operator === "+"){
+    console.log("the sum of two number is ", num1 + num2);
+}else if(operator === "-"){
+    console.log("the sub of two number is ", num1 - num2);
+}else if(operator === "*"){
+    console.log("the multiply of two number is ", num1*num2 );
+}else if(operator === "/"){
+    console.log("the division of two number is ", num2 / num1);
+}
