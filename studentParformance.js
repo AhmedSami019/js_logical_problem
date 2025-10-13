e16fccf368a3c2869ba59f7c1e1a1f3ec57cb284
AhmedSami019 <easami019192@gmail.com>
2025-10-13 19:59:22 +0600

    student parformance check problem solved

---
+++ studentParformance.js
@@ -0,0 +1,11 @@
+const mark = 25;
+
+if(mark >= 90){
+    console.log("Excellent");
+}else if(mark >= 75){
+    console.log("very good");
+}else if(mark >= 50){
+    console.log("Average");
+}else{
+    console.log("need improvement");
+}
